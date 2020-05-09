c63a92af709720fec7acc8a26a15f4fb809e9f5e
ACSBSC <36998939+ACSBSC@users.noreply.github.com>
2020-05-09 18:30:56 -0500

    Initial commit

---
+++ app/src/main/java/com/example/android_project/InfoFactory.kt
@@ -0,0 +1,12 @@
+package com.example.android_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InfoFactory : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_info_factory)
+    }
+}
